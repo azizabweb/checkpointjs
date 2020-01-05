4af4160c61f62adab2f7f9d2fb9c6e45a9ff3e37
azizabweb <58663211+azizabweb@users.noreply.github.com>
2020-01-05 17:14:20 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,47 @@
+var mytag = document.getElementById("myheader");
+var myfeedbacktag = document.getElementById("center");
+var texttag = document.getElementById("text");
+
+function makeitbold() {
+    if (texttag.style.fontWeight != "bold") {
+        texttag.style.fontWeight = "bold";
+
+    } else {
+        texttag.style.fontWeight = "normal";
+    }
+
+}
+
+function makeititalic() {
+    if (texttag.style.fontStyle != "italic") {
+        texttag.style.fontStyle = "italic"
+
+    } else {
+
+        texttag.style.fontStyle = "normal"
+    }
+}
+
+function makeitunderline() {
+    if (texttag.style.textDecoration != "underline") {
+        texttag.style.textDecoration = "underline"
+    } else {
+        texttag.style.textDecoration = "none"
+    }
+}
+
+function changesize() {
+    var userfontsize = document.getElementById("selectsize").value;
+    texttag.style.fontSize = userfontsize;
+}
+
+function fontfamily() {
+    var userfontfamily = document.getElementById("selecttype").value;
+    texttag.style.fontFamily = userfontfamily;
+}
+
+function makeitred() {
+
+    mytag.style.color = "red";
+    myfeedbacktag.style.color = "blue";
+}
